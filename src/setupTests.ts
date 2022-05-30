f2b9ddb516fef2f009dbf58791a9f51e3e762804
Raman Bedi <raman.bedi@trantorinc.com>
2022-05-30 18:52:47 +0530

    Add command for coverage

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Fix for Jest to detect matchMedia
+global.matchMedia = global.matchMedia || (() => {
+    return {
+        matches : false,
+        addListener : () => {},
+        removeListener: () => {}
+    }
+})
